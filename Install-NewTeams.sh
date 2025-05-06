948f82b5bc78b0b65a374504e1cbb626b413c32b
The Knox Tech <119535757+theknoxtech@users.noreply.github.com>
2025-05-06 09:09:15 -0400

    Add Install-macOSNewTeams.sh

---
+++ Install-NewTeams.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Check for Teams app
+if [ ! -d "/Applications/Microsoft Teams.app" ]; then
+
+      echo "Downloading MicrosoftTeams.pkg"
+
+      curl -L -o /tmp/MicrosoftTeams.pkg https://statics.teams.cdn.office.net/production-osx/enterprise/webview2/lkg/MicrosoftTeams.pkg
+
+      echo "Installing Microsoft Teams"
+
+      installer -pkg /tmp/MicrosoftTeams.pkg /Applications
+
+      echo "SUCCESS: Microsoft Teams has been installed"
+
+elif [ -d "/Applications/Microsoft Teams.app" ]; then
+
+    echo "FAILURE: Teams is already installed on the target Mac, please use Uninstall-NewTeams before running this script"
+
+fi
